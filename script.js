6d49c032529a6761f1dcee3e1ee1d44f3d4c9ada
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 16:52:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str: string, subStr: string): number {
+    // Convert both strings to lowercase for case-insensitive comparison
+    const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
+
+    // Find the index of the first occurrence of subStr in lowerStr
+    const index = lowerStr.indexOf(lowerSubStr);
+
+    return index;
 }
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// Examples
+console.log(indexOfIgnoreCase("Hello World", "world")); // Output: 6
+console.log(indexOfIgnoreCase("apple", "Ple"));         // Output: 2
+console.log(indexOfIgnoreCase("test", "aaa"));          // Output: -1
